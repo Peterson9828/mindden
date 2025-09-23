88f4c61f8cf24d1c102ad1b6b39256e69c985f5e
ZappMe <zappmecontact@gmail.com>
2025-09-23 15:33:26 +0200

    feat: add testing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,7 @@ export default defineNuxtConfig({
     "@nuxt/test-utils",
     "@nuxtjs/tailwindcss",
     "@pinia/nuxt",
+    "@nuxt/test-utils/module",
   ],
   pinia: {
     storesDirs: ["./stores/**", "app/stores/**"],
